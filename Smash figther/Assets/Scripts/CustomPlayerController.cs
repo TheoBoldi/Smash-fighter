1899f4546af5fb15eda7f32d59bc8f170cbe31b3
Théo <theo.boldireff@edu.devinci.fr>
2023-08-09 17:29:25 -0400

    Controller Update

--- Smash figther/Assets/Scripts/CustomPlayerController.cs
+++ Smash figther/Assets/Scripts/CustomPlayerController.cs
@@ -5,7 +5,7 @@ using Rewired;
 
 public class CustomPlayerController : MonoBehaviour
 {
-    private PlayerEntity _entity;
+    private PlayerController _entity;
     private Player _rewiredPlayer;
 
     public string playerName = "Player1";
@@ -13,34 +13,13 @@ public class CustomPlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _entity = GetComponentInChildren<PlayerEntity>();
+        _entity = GetComponentInChildren<PlayerController>();
         _rewiredPlayer = ReInput.players.GetPlayer(playerName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // float dirX = _rewiredPlayer.GetAxis("MoveX");
-        // float dirY = _rewiredPlayer.GetAxis("MoveY");
-        //
-        // Vector2 moveDir = new Vector2(dirX, dirY);
-        // moveDir.Normalize();
-        //
-        // _entity.Move(moveDir);
-        // Debug.Log(moveDir);
-        
-        float dirX = _rewiredPlayer.GetAxis("MoveX");
-        if (dirX != 0f)
-        {
-            Debug.Log(dirX);
-            dirX = Mathf.Sign(dirX);
-        }
-        
-        _entity.Move(dirX);
-        
-        if (_rewiredPlayer.GetButtonDown("Jump") && _entity.IsOnground())
-        {
-            _entity.Jump();
-        }
+
     }
 }
